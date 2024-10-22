187f1ed43566ed5d599246abfcec52d107251d63
Youjie Zheng <zyj578335934@gmail.com>
2024-10-22 10:40:32 +0800

    [feat] Update fork when the original repo unmatches the sync_list

--- sync.py
+++ sync.py
@@ -5,9 +5,9 @@ for line in f.readlines():
     if len(line) == 0 or line.strip() == '':
         continue
     print('Mirroring ' + line.strip());
-    # remote repo full_name, format: {{owner}}/{{repo}}
+    # # remote repo full_name, format: {{owner}}/{{repo}}
     remote_repo = line.strip()
-    # get repository name from remote repo full_name
+    # # get repository name from remote repo full_name
     repo_name = remote_repo.split("/")[-1]
 
     # the result of running sync command
@@ -17,10 +17,37 @@ for line in f.readlines():
         "sync", 
         "kern-crates/" + repo_name
     ], capture_output=True, text=True)
+    need_fork = sync_result.stderr.startswith("GraphQL: Could not resolve to a Repository with the name")
 
-    print(sync_result.stderr)
-    # # If the repo not exists, fork it form remote.
-    if sync_result.stderr.startswith("GraphQL: Could not resolve to a Repository with the name"):
+    if need_fork == False:
+    # Check whether the repo' parent is the given remote repo
+        need_fork = False
+        parent_result = subprocess.run([
+            "gh", 
+            "repo", 
+            "view", 
+            "kern-crates/" + repo_name, 
+            "--json", 
+            "parent"
+        ], capture_output=True, text=True)
+    
+        # Parse the parent result with json
+        import json
+        parent_owner= json.loads(parent_result.stdout)["parent"]["owner"]["login"]
+        parent_repo = json.loads(parent_result.stdout)["parent"]["name"]
+
+        if parent_owner + "/" + parent_repo != remote_repo:
+            # Delete the repository and fork it again
+            subprocess.run([
+                "gh", 
+                "repo", 
+                "delete", 
+                "kern-crates/" + repo_name, 
+                "--yes"
+            ])
+            need_fork = True
+
+    if need_fork:
         # fork the repository
         subprocess.run([
             "gh", 
@@ -31,4 +58,3 @@ for line in f.readlines():
             "kern-crates", 
             "--default-branch-only"
         ], capture_output=True, text=True)
-        
