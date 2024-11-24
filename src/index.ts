41e65683183a78e0897173039ab13f387e712169
zjp <jiping_zhou@foxmail.com>
2024-11-24 19:35:10 +0800

    replace ts-node  with tsx

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import { Octokit, App } from "octokit";
-
-function gh_token() {
-  const token = process.env.GH_TOKEN ?? process.env.GITHUB_TOKEN;
-  if (!token) {
-    throw new Error("请提供 GH_TOKEN 或者 GITHUB_TOKEN，比如 `GH_TOKEN: ${{ github.token }}`");
-  }
-  return token;
-}
+import { gh_token } from "./query.ts";
 
 async function main() {
   const octokit = new Octokit({ auth: gh_token() });
---
+++ src/query.ts
@@ -0,0 +1,9 @@
+//*
+//
+export function gh_token() {
+  const token = process.env.GH_TOKEN ?? process.env.GITHUB_TOKEN;
+  if (!token) {
+    throw new Error("请提供 GH_TOKEN 或者 GITHUB_TOKEN，比如 `GH_TOKEN: ${{ github.token }}`");
+  }
+  return token;
+}
