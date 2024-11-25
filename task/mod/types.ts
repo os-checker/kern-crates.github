078acb6840584a7dd7f89449dd6fd6e1df31fd9e
zjp <jiping_zhou@foxmail.com>
2024-11-25 18:26:44 +0800

    do_: replace async exec with execSync

--- task/mod/types.ts
+++ task/mod/types.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import { log } from "node:console";
 import * as query from "./query.ts";
-import { exec, ExecException } from "node:child_process";
+import { execSync } from "node:child_process";
 
 export type UserRepo = {
   user: string,
@@ -80,35 +80,31 @@ function do_fork(owner: string, outer: UserRepo) {
   return do_(cmd);
 }
 
+// gh CLI writes data to TTY but not to pipe, so even if we get output from terminal,
+// nothing is captured in the callback.
+//
+// But we can use inherit stdio to capture the gh output.
 function do_(cmd: string) {
   let success = true;
   console.time(cmd);
 
   if (process.env.EXECUTE === "true") {
-    log(chalk.yellow(`[real exec] ${cmd}`));
+    log(chalk.yellow(`\n[real exec] ${cmd}`));
 
-    const child = exec(cmd, (error, stdout, stderr) => handleExecOutput(cmd, error, stdout, stderr));
-
-    if (child.exitCode === null) {
-      log(cmd, ": is still running");
+    try {
+      execSync(cmd, { stdio: 'inherit' });
+    } catch (error) {
       success = false;
-    } else if (child.exitCode !== 0) {
-      log(cmd, ": exited with", child.exitCode);
-      success = false;
+      let err: any = error;
+      handleExecOutput(cmd, err, err.stdout, err.stderr);
     }
-  } else {
-    log(chalk.gray(`[fake exec] ${cmd}`));
-  }
 
-  console.timeEnd(cmd);
-  return success;
+    console.timeEnd(cmd);
+    return success;
+  }
 }
 
-function handleExecOutput(cmd: string, error: ExecException | null, stdout: string, stderr: string) {
-  // gh CLI writes data to TTY but not to pipe, so even if we get output from terminal,
-  // nothing is captured in the callba the callback.
-  // See https://stackoverflow.com/questions/72731726/how-do-i-capture-output-of-the-following-command-gh-workflow-run-id
-  // and https://stackoverflow.com/questions/78316928/gh-has-different-output-when-captured-with-python-subprocess-run
+function handleExecOutput(cmd: string, error: any, stdout: string, stderr: string) {
   if (stdout) { log(`${cmd} [stdout]: ${stdout}`); }
   if (error) {
     console.error(chalk.bgRed(`${cmd} 执行出错:\n`) + chalk.red(`${stderr}`));
@@ -116,3 +112,4 @@ function handleExecOutput(cmd: string, error: ExecException | null, stdout: stri
     console.error(`${cmd} [stderr]: ${stderr}`);
   }
 }
+
